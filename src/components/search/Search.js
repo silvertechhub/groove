729f2b4d7854a476aa5131b923bc129e8c9c4f19
silvertechhub <agubatasylvester@gmail.com>
2022-12-08 22:15:14 +0100

    first commit

---
+++ src/components/search/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './search.css'
+
+export default function Search() {
+  return (
+    <div>
+        <input type='text' placeholder='search'/>
+        bbb
+    </div>
+  )
+}
---
+++ src/pages/DashBoard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Search from '../components/search/Search'
+import SideNav from '../components/sideNav/SideNav'
+import './dashboard.css'
+
+export default function DashBoard() {
+  return (
+    <div >
+     <SideNav />
+     <Search />
+     </div>
+  )
+}
